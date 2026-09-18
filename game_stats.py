b4c449edbb78dcf86922af0cec433fee69351ab1
SzymanskaRoza <rozaszymanska2@gmail.com>
2026-09-18 13:38:44 +0200

    Add alien collisions and game over

---
+++ game_stats.py
@@ -0,0 +1,11 @@
+class GameStats:
+    """Track statistics for Alien Invasion."""
+
+    def __init__(self, alien_invasion_game):
+        """Initialize statistics."""
+        self.settings = alien_invasion_game.settings
+        self.reset_stats()
+
+    def reset_stats(self):
+        """Initialize statistics that can change during the game."""
+        self.ships_left = self.settings.ship_limit
